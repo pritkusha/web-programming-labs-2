085ffa999db7bca1e8b5040d7fc4cb3ee4792e6a
Прыткова <prytkova.sofi@gmail.com>
2024-11-22 20:19:44 +0700

    Арифметика

--- lab4.py
+++ lab4.py
@@ -25,3 +25,88 @@ def div():
     result = x1 / x2
     return render_template('lab4/div.html', x1=x1, x2=x2, 
                            result=result)
+
+@lab4.route('/lab4/sum-form')
+def sum_form():
+    return render_template('lab4/sum-form.html')
+
+@lab4.route('/lab4/sum', methods = ['POST'])
+def sum():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = 0
+    else:
+        x1 = int(x1)
+    if x2 == '':
+        x2 = 0
+    else:
+        x2 = int(x2)
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 + x2
+    return render_template('lab4/sum.html', x1=x1, x2=x2, 
+                           result=result)
+
+@lab4.route('/lab4/multiply-form')
+def multiply_form():
+    return render_template('lab4/smultiply-form.html')
+
+@lab4.route('/lab4/multiply', methods = ['POST'])
+def multiply():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '':
+        x1 = 1
+    else:
+        x1 = int(x1)
+    if x2 == '':
+        x2 = 1
+    else:
+        x2 = int(x2)
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 * x2
+    return render_template('lab4/multiply.html', x1=x1, x2=x2, 
+                           result=result)
+
+@lab4.route('/lab4/sub-form')
+def sub_form():
+    return render_template('lab4/sub-form.html')
+
+@lab4.route('/lab4/sub', methods = ['POST'])
+def sub():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/sub.html', 
+                               error='Оба поля должны быть заполнены!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 - x2
+    return render_template('lab4/sub.html', x1=x1, x2=x2, 
+                           result=result)
+
+@lab4.route('/lab4/expon-form')
+def expon_form():
+    return render_template('lab4/expon-form.html')
+
+@lab4.route('/lab4/expon', methods = ['POST'])
+def expon():
+    x1 = request.form.get('x1')
+    x2 = request.form.get('x2')
+    if x1 == '' or x2 == '':
+        return render_template('lab4/expon.html', 
+                               error='Оба поля должны быть заполнены!')
+    if x1 == 0 and x2 == 0:
+        return render_template('lab4/expon.html', 
+                               error2='Оба поля не должны быть нулевые!')
+
+    x1 = int(x1)
+    x2 = int(x2)
+    result = x1 ** x2
+    return render_template('lab4/expon.html', x1=x1, x2=x2, 
+                           result=result)
