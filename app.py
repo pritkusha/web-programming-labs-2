4e87f6eaef1b4bd8efcabdb177e32e8043b11359
Прыткова <prytkova.sofi@gmail.com>
2024-12-21 04:10:03 +0700

    Лабораторная работа 9

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 from flask import Flask, redirect, url_for, render_template, abort, request
+from os import path
+import os
+from flask_sqlalchemy import SQLAlchemy
+from db import db
+
 from lab1 import lab1 
 from lab2 import lab2
 from lab3 import lab3
@@ -7,10 +12,28 @@ from lab5 import lab5
 from lab6 import lab6
 from lab7 import lab7
 from lab8 import lab8
+from lab9 import lab9
 
 app = Flask(__name__)
 
-app.secret_key = 'секретно-секретный секрет'
+#app.secret_key = 'секретно-секретный секрет'
+
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
+
+if app.config['DB_TYPE'] == 'postgres':
+    db_name = "sofiya_prytkova_orm"
+    db_user = "sofiya_prytkova_orm"
+    db_password = "123"
+    host_ip = "127.0.0.1"
+    host_port = 5433
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+else:
+    dir_path = path.dirname(path.realpath(__file__))
+    db_path = path.join(dir_path, "sofiya_prytkova_orm.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
+
+db.init_app(app)
 
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
@@ -20,6 +43,7 @@ app.register_blueprint(lab5)
 app.register_blueprint(lab6)
 app.register_blueprint(lab7)
 app.register_blueprint(lab8)
+app.register_blueprint(lab9)
 
 @app.route("/")
 @app.route("/index")
@@ -51,6 +75,7 @@ def menu():
                 <li><a href="/lab6">Шестая лабораторная</a></li>
                 <li><a href="/lab7">Седьмая лабораторная</a></li>
                 <li><a href="/lab8">Восьмая лабораторная</a></li>
+                <li><a href="/lab9">Девятая лабораторная</a></li>
             </ul>
         </nav>
 
@@ -60,7 +85,6 @@ def menu():
     </body>
 </html>
 """
-
 
 # Обработчик ошибки 404
 @app.errorhandler(404)
